ae8249300cf50333470b9c4945b0cef9fae6693a
vignesh-vicky-tech <85112276+vignesh-vicky-tech@users.noreply.github.com>
2024-03-27 18:48:22 +0530

    Delete login-developer1.sh

--- login-developer1.sh
+++
@@ -1,8 +0,0 @@
-
-
-export HOST=$(hostname -I | awk '{print $1}')
-            
-sudo argocd login $HOST:32145 --username admin --password Password@1 --insecure
-
-echo 'Fetching argocd version'
-sudo argocd version
